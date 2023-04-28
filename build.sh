3c8c265a317691244c61156af7bbb50499d96b55
Paulo Abrantes <paulo.abrantes@qub-it.com>
2023-04-28 14:00:17 +0100

    Moving from package to install

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --rm -v $(pwd)/m2:/root/.m2 -v $(pwd):/test -w /test maven:3.8.6-openjdk-8-slim mvn clean package -Dmaven.test.skip=true -Dmaven.javadoc.skip=true
+docker run --rm -v $(pwd)/m2:/root/.m2 -v $(pwd):/test -w /test maven:3.8.6-openjdk-8-slim mvn clean install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true
 
